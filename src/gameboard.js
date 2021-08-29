5bc5bf928f352e054936f678c349fe2ed020de44
Ryan Maloney <rypmaloney@gmail.com>
2021-08-29 16:28:25 -0400

    Cleaned up and organized gameboard tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,70 +1,64 @@
 import { node } from "webpack";
-import ship from "./ship"
+import ship from "./ship";
 
 const gameboard = (player) => {
-    //information about the gameboard
-    const meta = {
-        playerName: player,
-        ships: [],
-        missedHits: [],
+  //information about the gameboard
+  const meta = {
+    playerName: player,
+    ships: [],
+    missedHits: [],
+  };
+  //creation of the gameboard through looping; 10x10; use board[x][y] to reference a space
+  let board = [];
+  for (let i = 0; i < 10; i++) {
+    board.push([]);
+    for (let x = 0; x < 10; x++) {
+      board[i].push({
+        ship: false,
+        id: null,
+        hit: false,
+        missedHit: false,
+      });
     }
-    //creation of the gameboard through looping; 10x10; use board[x][y] to reference a space
-    let board = []
-    for (let i = 0; i < 10; i ++){
-        board.push([])
-        for (let x = 0; x < 10; x ++){
-            board[i].push({
-                ship:false,
-                id: null,
-                hit: false,
-                missedHit:false,
-            })
-            
-        }
+  }
+  //Board giveth
+  const getBoard = () => board;
+
+  //
+  function placeShip(x, y, length) {
+    let id = `${x}x${y}`;
+    board[x][y].ship = true;
+    board[x][y].id = id;
+
+    meta.ships.push(ship(length, x, y));
+
+    //only works for vertical ships
+    for (let i = 0; i < length - 1; i++) {
+      y -= 1;
+      board[x][y].ship = true;
+      board[x][y].id = id;
     }
-    //Board giveth
-    const getBoard = () => board;
+  }
 
-    //
-    function placeShip(x, y, length){
-        let id = `${x}x${y}`
-        board[x][y].ship = true
-        board[x][y].id = id
+  function receiveAttack(x, y) {
+    if (board[x][y].id != null) {
+      //find the id of the boat at that location in the meta.ships array
+      let index = meta.ships.map((e) => e.id).indexOf(board[x][y].id);
 
-        meta.ships.push(ship(length,x,y))
-        
-
-        //only works for vertical ships 
-        for(let i=0;i<length-1; i++){
-            y-=1
-            board[x][y].ship = true;
-            board[x][y].id = id
-        }
+      //hit that boat at the location
+      meta.ships[index].hit(x, y);
+    } else {
+      meta.missedHits.push([x, y]);
+      board[x][y].missedHit = true;
     }
+  }
 
-    function receiveAttack(x,y){
-        if(board[x][y].id != null){
-            //find the id of the boat at that location in the meta.ships array
-            let index = meta.ships.map(e => e.id).indexOf(board[x][y].id)
+  return {
+    getBoard,
+    meta,
+    placeShip,
+    receiveAttack,
+  };
+};
 
-            //hit that boat at the location
-            meta.ships[index].hit(x,y)
-            
-        }else{
-            meta.missedHits.push([x, y])
-            board[x][y].missedHit=true
-
-        }
-    }
-
-
-
-    return {
-        getBoard, 
-        meta,
-        placeShip,
-        receiveAttack
-            }
-}
-
-export default gameboard;   
+export default gameboard;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,62 +1,74 @@
-import gameboard from '../gameboard'
-import ship from '../ship'
+import gameboard from "../gameboard";
+import ship from "../ship";
 
-let player = "me"
-let playerBoard = gameboard(player)
-let board = playerBoard.getBoard()
-test('Tests existence of board', () => {
+describe("Is the board and a ship there?", () => {
+  let player = "me";
+  let playerBoard = gameboard(player);
+  let board = playerBoard.getBoard();
+  test("Tests existence of board", () => {
     expect(board.length).toBe(10);
-});
+  });
 
-playerBoard.placeShip(2,3,1)
-test('Tests existence of ship on board', () => {
+  playerBoard.placeShip(2, 3, 1);
+  test("Tests existence of ship on board", () => {
     expect(board[2][3].ship).toBe(true);
-});
+  });
 
-playerBoard.placeShip(2,3,1)
-test('confirms existence of ship on board', () => {
+  playerBoard.placeShip(2, 3, 1);
+  test("confirms existence of ship on board", () => {
     expect(board[2][4].ship).toBe(false);
-});
+  });
 
-playerBoard.placeShip(4,3,3)
-test('confirms placement of 4 square vertical ship', () => {
+  playerBoard.placeShip(4, 3, 3);
+  test("confirms placement of 4 square vertical ship", () => {
     expect(board[4][3].ship).toBe(true);
-});
-test('confirms placement of 4 square vertical ship partb', () => {
+  });
+  test("confirms placement of 4 square vertical ship partb", () => {
     expect(board[4][2].ship).toBe(true);
+  });
+
+  test("confirms id is present", () => {
+    expect(board[4][1].id).toBe("4x3");
+  });
 });
 
-test('confirms id is present', () => {
-    expect(board[4][1].id).toBe('4x3');
-});
-
-let samplePlayerShip = ship(3,3,3)
-let samplePlayerBoard = gameboard(player)
-samplePlayerBoard.meta.ships.push(samplePlayerShip)
 
 
-test('confirms ship manifest logs information', () => {
+describe("checking logging of hits on board and ships", () => {
+  let samplePlayerShip = ship(3, 3, 3);
+  let samplePlayerBoard = gameboard('player');
+  samplePlayerBoard.meta.ships.push(samplePlayerShip);
+
+  test("confirms ship manifest logs information", () => {
     expect(samplePlayerBoard.meta.ships.length).toBe(1);
-});
+  });
 
+  let newPlayerBoard = gameboard("me");
+  let ship1 = ship(3, 3, 3);
+  newPlayerBoard.placeShip(3, 3, 3);
+  let ship2 = ship(5, 7, 2);
+  newPlayerBoard.placeShip(5, 7, 2);
 
-let  newPlayerBoard = gameboard(player)
-let ship1 = ship(3,3,3)
-newPlayerBoard.placeShip(3,3,3)
-let ship2 = ship(5,7,2)
-newPlayerBoard.placeShip(5,7,2)
+  newPlayerBoard.receiveAttack(3, 3);
 
-newPlayerBoard.receiveAttack(3,3)
-
-
-test('confirms receive attack logs hit on appropriate ship', () => {
+  test("confirms receive attack logs hit on appropriate ship", () => {
     expect(newPlayerBoard.meta.ships[0].hits.length).toBe(1);
+  });
 });
-/*
-newPlayerBoard.receiveAttack(3,2)
-newPlayerBoard.receiveAttack(3,1)
 
-test('confirms sinking of a ship', () => {
+
+describe("does sinking work in meta.ships", () => {
+    let newPlayerBoard = gameboard("me");
+    let ship1 = ship(3, 3, 3);
+    newPlayerBoard.placeShip(3, 3, 3);
+    let ship2 = ship(5, 7, 2);
+    newPlayerBoard.placeShip(5, 7, 2);
+  
+    newPlayerBoard.receiveAttack(3, 3);
+  newPlayerBoard.receiveAttack(3, 2);
+  newPlayerBoard.receiveAttack(3, 1);
+
+  test("confirms sinking of a ship", () => {
     expect(newPlayerBoard.meta.ships[0].isSunk()).toBe(true);
+  });
 });
-*/
