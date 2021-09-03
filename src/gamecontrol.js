5984f65a79efced04c653aebb420b9bdfb6b50f4
Ryan Maloney <rypmaloney@gmail.com>
2021-09-02 21:10:42 -0400

    Created update board function. Added event listener and click event for computer board

--- src/gamecontrol.js
+++ src/gamecontrol.js
@@ -2,15 +2,59 @@ import player from './player'
 import gameboard from './gameboard'
 import ship from './ship'
 
+/* Steps:
+    1. Initialize gameboard x
+    2. Computer picks random ships
+    3. Player places their ships:
+        1.
+    4. Turns begin. Each turn consits of:
+        1. choosing spot runnign computerBoard.receive(spot)
+        2. Switch turn
+
+*/
+
+
+function turn(e){
+    computerGameboard.receiveAttack(parseInt(e.target.id))
+    updateBoard(e)
+}
+
+
+
+
+
+
+
+
+
+
 const playerGameboard = gameboard('player')
 const computerGameboard = gameboard('computer')
 
 const computerDomBoard = document.getElementById('computer-board');
 const playerDomBoard = document.getElementById('player-board');
 
+
+function updateBoard(e){
+    let index = e.target.id
+
+    let cboard = computerGameboard.getBoard()
+    let spot = document.getElementById(index)
+    if(cboard[index].ship == true){spot.setAttribute('class', "ship")}
+    if(cboard[index].hit == true){spot.setAttribute('class', "hit")}
+    if(cboard[index].missedHit == true){spot.setAttribute('class', "miss")}
+}
+
+
+
+
 //two test ships
 playerGameboard.placeShip(50, 4)
 playerGameboard.placeShip(75, 2)
+computerGameboard.placeShip(50, 4)
+computerGameboard.placeShip(75, 2)
+computerGameboard.placeShip(98, 5)
+computerGameboard.placeShip(33, 2)
 
 //testing hit and miss
 playerGameboard.receiveAttack(40)
@@ -22,7 +66,10 @@ function initializeBoard(){
     for (let i=0 ; i < cboard.length; i++){
         let spot = document.createElement('div');
         spot.setAttribute('id', i)
-        spot.setAttribute('class', "spot")
+        spot.setAttribute('class', "spot computer-spot")
+
+        spot.addEventListener("click",(e)=> turn(e));
+
         computerDomBoard.appendChild(spot)
     }
     //create player board
